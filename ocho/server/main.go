1901ee4899e18156ae5d7ddeaa5a6ae364a5a912
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-04-24 19:54:26 +0200

    dia trece

--- ocho/server/main.go
+++ ocho/server/main.go
@@ -7,9 +7,13 @@ import (
 	"fmt"
 )
 
-func getFarenheit(c *gin.Context) {
+func parseValue(c *gin.Context) (float64, error) {
 	value := c.Param("value")
-	celsius, err := strconv.ParseFloat(value, 64)
+	return strconv.ParseFloat(value, 64)
+}
+
+func getFarenheit(c *gin.Context) {
+	celsius, err := parseValue(c)
 	if err != nil {
 		fmt.Println("Errrrr", err)
 		c.String(400, "Incorrect value")
@@ -20,9 +24,21 @@ func getFarenheit(c *gin.Context) {
 	c.String(200, "Value %s", farenheit)
 }
 
+func getCelsius(c *gin.Context) {
+	farenheit, err := parseValue(c)
+	if err != nil {
+		fmt.Println("Errrrr", err)
+		c.String(400, "Incorrect value")
+		return 
+	}
+
+	celsius := conversions.IntoCelsius(conversions.Farenheit(farenheit))
+	c.String(200, "Value %s", celsius)
+}
+
 func main() {
 	server := gin.Default()
 	server.GET("/farenheit/:value", getFarenheit)
-
+	server.GET("/celsius/:value", getCelsius)
 	server.Run("localhost:8081")
 }
